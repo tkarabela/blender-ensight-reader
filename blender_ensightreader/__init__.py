339d6b0bce7d066f05b62996fa8697c1daea8463
Tomas Karabela <tkarabela@seznam.cz>
2022-04-03 22:20:46 +0200

    Add license, clarify support for Blender 2.93+

--- blender_ensightreader/__init__.py
+++ blender_ensightreader/__init__.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2022 Tomas Karabela
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
 try:
     from .importer import ImportEnsightGold
     import bpy
@@ -11,7 +32,7 @@ bl_info = {
     "author": "Tomas Karabela",
     "description": "Imports surface geometry from EnSight Gold case.",
     "version": (1, 0, 0),
-    "blender": (2, 80, 0),
+    "blender": (2, 93, 0),
     "location": "File > Import > EnSight Gold",
     "url": "https://github.com/tkarabela/ensight-reader",
     "wiki_url": "https://github.com/tkarabela/ensight-reader",
